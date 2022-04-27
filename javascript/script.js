95f0a6860e326b62643f793bfd20a24eca8e576c
Louise Ballantyne <lousballantyne@icloud.com>
2022-04-27 08:38:36 +0100

    Updated to not count today

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,7 @@
 $( document ).ready (function() {
-  const date = Date.now()
+  const today = new Date()
+  const tomorrow = new Date(today)
+  tomorrow.setDate(tomorrow.getDate() + 1)  
   const conferenceDate = '2022-05-15'
   function countCertainDays(days, d0, d1 ) {
     var ndays = 1 + Math.round((d1-d0)/(24*3600*1000));
@@ -7,7 +9,7 @@ $( document ).ready (function() {
       return a + Math.floor( (ndays+(d0.getDay()+6-b) % 7 ) / 7 ); };
     return days.reduce(sum,0);
   }
-  const toadsTillConference = countCertainDays([3], new Date(date), new Date(conferenceDate))
+  const toadsTillConference = countCertainDays([3], new Date(tomorrow), new Date(conferenceDate))
         $('#toads').text( `${toadsTillConference} toads till conference` );
 
         $( '#toad-img' ).hover(
